5da58db644f312f3c840ee0b600891a3496329ab
2hf <zh.f@outlook.com>
2015-07-23 20:54:37 +0800

    first commit
    codes commit

---
+++ HelloSwift/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  HelloSwift
+//
+//  Created by Fan Zhang on 7/23/15.
+//  Copyright (c) 2015 ZH.F. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMessage(){
+        let alertController = UIAlertController(title: "Welcome to my first app", message: "Hello Swift", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+        
+        
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Preface
+
+> "The way to get started is to quit talking and begin doing." -- Walt Disney
+
+# About
+
+Recently I find this book "Beginning iOS 8 Programming with Swift" is awsome. While learning by doing, I am going to commit my codes into this repository. At the same time, my "Contributions" to this repository can encourage me to keep moving.
+
+**Let's get my feet wet with iOS and Swift! It's all about having fun with "hello swift"!**
